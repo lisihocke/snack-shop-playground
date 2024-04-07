31bbe84965f14c73057495aedbbb2a0e67e92f0c
lisihocke <lisihocke@users.noreply.github.com>
2024-04-07 23:17:15 +0200

    Use React Aria's Tailwind starter kit components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
-import {Button, Link} from 'react-aria-components'
+import {Link} from './components/Link'
+import {Button} from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,8 +13,23 @@ function App() {
           Hello world!
         </h1>
       <div className="activeButton">
-        <Button className="btn-primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <Button variant="primary" onClick={() => setCount((count) => count + 1)}>
+          primary count is {count}
+        </Button>
+      </div>
+      <div className="activeButton">
+        <Button variant="secondary" onClick={() => setCount((count) => count + 1)}>
+          secondary count is {count}
+        </Button>
+      </div>
+      <div className="activeButton">
+        <Button variant="destructive" onClick={() => setCount((count) => count + 1)}>
+          destructive count is {count}
+        </Button>
+      </div>
+      <div className="activeButton">
+        <Button variant="icon" onClick={() => setCount((count) => count + 1)}>
+          icon count is {count}
         </Button>
       </div>
       <div className="disabledButton">
